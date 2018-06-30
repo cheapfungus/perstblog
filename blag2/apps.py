737b3687bef79969d9d576b654b81761bc9a0157
Cheapfungus <cheapfungus@gmail.com>
2018-06-30 13:32:43 +0800

    Changed the HTML for the site.

---
+++ blag2/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Blag2Config(AppConfig):
+    name = 'blag2'
---
+++ blag2/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+def post_list(request):
+		return render(request,'blag2/post_list.html',{})
+
+# Create your views here.
